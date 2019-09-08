bae7554dd39ffdf4e695a1e3650703dd6ee329ba
Enrico Beyer <enrico@friendventure.de>
2019-09-08 10:34:21 +0200

    Initial commit

---
+++ web/app/themes/theme/resources/lib/timber.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+include 'timber/error.php';
+include 'timber/directories.php';
+include 'timber/setup.php';
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
---
+++ web/app/themes/theme/resources/lib/timber/directories.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+Timber::$dirname = array(
+	'views',
+	'views/templates',
+	'views/templates/modules',
+	'views/templates/objects',
+	'views/templates/partials',
+	'views/templates/wordpress'
+);
